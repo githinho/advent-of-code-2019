bfd1421f5dceb8587b74d6f2f3886519bbabb19e
Eugen Druzin <edruzin@gmail.com>
2019-12-02 23:49:52 +0100

    Finalise day2

--- day2/day2.exs
+++ day2/day2.exs
@@ -1,19 +1,28 @@
-beforeRunning = fn (code) ->
-  List.replace_at(code, 1, 12) |> List.replace_at(2, 2)
-end
-
 defmodule Execute do
-  def code(code, index \\ 0) do
+  def executeInstructions(code, index \\ 0) do
     case Enum.at(code, index) do
       1 -> newValue = Enum.at(code, Enum.at(code, index + 1)) + Enum.at(code, Enum.at(code, index + 2))
         newList = List.replace_at(code, Enum.at(code, index + 3), newValue)
-        Execute.code(newList, index + 4)
+        Execute.executeInstructions(newList, index + 4)
       2 -> newValue = Enum.at(code, Enum.at(code, index + 1)) * Enum.at(code, Enum.at(code, index + 2))
         newList = List.replace_at(code, Enum.at(code, index + 3), newValue)
-        Execute.code(newList, index + 4)
+        Execute.executeInstructions(newList, index + 4)
       99 -> Enum.at(code, 0)
       _ -> IO.puts "Problem"
     end
+  end
+  def beforeRunning(code, noun, verb) do
+    List.replace_at(code, 1, noun) |> List.replace_at(2, verb)
+  end
+  def findSolution(instructions, result) do
+    Enum.each(0..99, fn (x) ->
+      Enum.each(0..99, fn (y) ->
+        value = instructions |> beforeRunning(x, y) |> executeInstructions()
+        if value == result do
+          IO.inspect 100 * x + y
+        end
+      end)
+    end)
   end
 end
 
@@ -22,8 +31,7 @@ case File.read("input.txt") do
     IO.inspect body
     |> String.split(",")
     |> Enum.map(&String.to_integer/1)
-    |> beforeRunning.()
-    |> Execute.code()
+    |> Execute.findSolution(19690720)
   {:error, reason} ->
     IO.puts reason
 end
