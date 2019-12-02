d94d3aebe8eb9764f633757bdae84e2e2aa6279c
Eugen Druzin <edruzin@gmail.com>
2019-12-02 21:13:25 +0100

    Finalise day1

--- day1/day1.exs
+++ day1/day1.exs
@@ -1,7 +1,19 @@
 
 defmodule Math do
+  def getFullFuel(number) do
+    fuel = getFuel(number)
+    fuel + getFuelForFuel(fuel)
+  end
   def getFuel(number) do
     trunc(div(number, 3)) - 2
+  end
+  def getFuelForFuel(number) do
+    fuel = getFuel(number)
+    case fuel do
+      x when x < 1 -> 0
+      x when x > 0 -> getFuelForFuel(fuel) + fuel
+      _ -> IO.puts "Error in calculating fuel for fuel"
+    end
   end
 end
 
@@ -10,7 +22,7 @@ case File.read("input.txt") do
     IO.inspect body
     |> String.split("\n", trim: true)
     |> Enum.map(&String.to_integer/1)
-    |> Enum.map(&Math.getFuel/1)
+    |> Enum.map(&Math.getFullFuel/1)
     |> Enum.sum()
   {:error, reason} ->
     IO.puts reason
